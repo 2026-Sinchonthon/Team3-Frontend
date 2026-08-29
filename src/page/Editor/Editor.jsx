44abbddcf1e542e8ab2e63e5d304ccd59d580b6f
busicoder <judge060217@gmail.com>
2026-08-29 16:08:29 +0900

    refactor: simplify editor actions

--- src/page/Editor/Editor.jsx
+++ src/page/Editor/Editor.jsx
@@ -65,16 +65,9 @@ const LocationDetail = styled.p`
   font-size: 0.875rem;
 `;
 
-const LocationButton = styled.button`
-  justify-self: start;
-  padding: 0.625rem 0.875rem;
-  border: 0.0625rem solid #d7d7d7;
-  border-radius: 0.5rem;
-  background: #fff;
-  cursor: pointer;
-`;
-
 const SaveButton = styled.button`
+  justify-self: start;
+  width: 8rem;
   height: 2.75rem;
   border: 0;
   border-radius: 0.5rem;
@@ -175,9 +168,6 @@ export default function Editor() {
           </LocationBox>
         </div>
 
-        <LocationButton type="button" onClick={() => navigate("/search")}>
-          위치 변경
-        </LocationButton>
         <SaveButton type="submit" disabled={isSaving}>
           저장
         </SaveButton>
