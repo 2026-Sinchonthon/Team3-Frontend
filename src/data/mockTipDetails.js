bf65ac3efbc97173180110716272d69f12b369c2
Min-hye-Kim <kimilu@ewha.ac.kr>
2026-08-29 17:31:40 +0900

    🤡 chore : Mock Data 추가

--- src/data/mockTipDetails.js
+++ src/data/mockTipDetails.js
@@ -1,3 +1,40 @@
+import { findCategory } from "../constants/categories";
+import { mockPlaces, mockTips } from "./mockTips";
+
+/*
+게시글 상세(TipFeed)용 목업입니다.
+
+홈 화면 바텀시트에서 넘어온 팁도 상세가 열리도록,
+지도용 목업(mockTips)에서 같은 데이터를 상세 형태로 파생시킵니다.
+API가 붙으면 이 파일은 통째로 지우고 TipAPI에서 서버 응답을 그대로 쓰면 됩니다.
+*/
+
+const placesById = new Map(mockPlaces.map((place) => [place.id, place]));
+
+function toTipDetail(tip) {
+  const place = placesById.get(tip.placeId);
+
+  return {
+    id: tip.id,
+    author: {
+      nickname: tip.author.nickname,
+      trustScore: tip.author.trustScore,
+    },
+    category: findCategory(tip.categoryId)?.tag ?? "",
+    title: tip.title,
+    content: tip.content,
+    location: place
+      ? {
+          id: place.id,
+          name: place.name,
+          address: place.address,
+          lat: place.latitude,
+          lng: place.longitude,
+        }
+      : null,
+  };
+}
+
 const mockTipDetails = [
   {
     id: -1,
@@ -16,6 +53,7 @@ const mockTipDetails = [
       lng: 126.94,
     },
   },
+  ...mockTips.map(toTipDetail),
 ];
 
 export default mockTipDetails;
